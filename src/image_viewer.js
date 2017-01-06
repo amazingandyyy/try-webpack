a345a729399a799f989a1450e94dd0e87b021bbb
Amazingandyyy <amazingandyyy@gmail.com>
2017-01-06 12:23:11 -0800

    add ignore

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,15 +1,17 @@
-import big from '../assets/big.jpg';
+// import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
 
-document.body.appendChild(image);
+    document.body.appendChild(image);
+}
 
+// const imageBig = document.createElement('img');
+// imageBig.src = big;
 
-const imageBig = document.createElement('img');
-imageBig.src = big;
+// document.body.appendChild(imageBig);
 
-document.body.appendChild(imageBig);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import sum from './sum';
-import './image_viewer';
+// import sum from './sum';
+// import './image_viewer';
 
-const total = sum(533, 4);
+// const total = sum(533, 4);
 
-console.log(total);
+// console.log(total);
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+
+button.onclick = () => {
+    System.import('./image_viewer').then(module => {
+        console.log(module.default);
+        module.default()
+    });
+}
+
+document.body.appendChild(button);
